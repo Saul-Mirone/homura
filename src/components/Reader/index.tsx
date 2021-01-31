9937a8131921c39014b696725a951768fc24aff0
Saul-Mirone <Saul.Mirone@gmail.com>
2021-02-01 00:59:53 +0800

    auto scroll to top when switch post

--- src/components/Reader/index.tsx
+++ src/components/Reader/index.tsx
@@ -13,29 +13,36 @@ type ReaderProps = {
     toolkit: JSX.Element | null;
 };
 
-export const Reader: React.FC<ReaderProps> = ({ post, toolkit }) => (
-    <div className="reader-container">
-        {post && toolkit}
+export const Reader: React.FC<ReaderProps> = ({ post, toolkit }) => {
+    const postContainer = React.useRef<HTMLDivElement>(null);
+    React.useEffect(() => {
+        postContainer.current?.scrollTo(0, 0);
+    }, [post]);
 
-        {post ? (
-            <div className="thin-scroll">
-                <div className="reader-post">
-                    <hgroup>
-                        <small>{post.date}</small>
-                        <div>{post.sourceName}</div>
-                        <h1>{post.title}</h1>
-                    </hgroup>
-                    <base href={post.link} />
-                    {/* eslint-disable-next-line react/no-danger */}
-                    <main dangerouslySetInnerHTML={{ __html: post.content }} />
+    return (
+        <div className="reader-container">
+            {post && toolkit}
+
+            {post ? (
+                <div ref={postContainer} className="thin-scroll">
+                    <div className="reader-post">
+                        <hgroup>
+                            <small>{post.date}</small>
+                            <div>{post.sourceName}</div>
+                            <h1>{post.title}</h1>
+                        </hgroup>
+                        <base href={post.link} />
+                        {/* eslint-disable-next-line react/no-danger */}
+                        <main dangerouslySetInnerHTML={{ __html: post.content }} />
+                    </div>
                 </div>
-            </div>
-        ) : (
-            <div className="reader-empty">
-                <div>
-                    <img alt="homura" src={homura} />
+            ) : (
+                <div className="reader-empty">
+                    <div>
+                        <img alt="homura" src={homura} />
+                    </div>
                 </div>
-            </div>
-        )}
-    </div>
-);
+            )}
+        </div>
+    );
+};
